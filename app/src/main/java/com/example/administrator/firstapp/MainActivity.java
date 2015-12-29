c1af76883c1b4bf69eced2d3735f4f94d183c573
ljgsonx <ljgsonx@163.com>
2015-12-29 22:12:38 +0800

    git test 2

--- app/src/main/java/com/example/administrator/firstapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/firstapp/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         init();
         loginBtn.setOnClickListener(cl);
         loginCancel.setOnClickListener(cl);
+        //fot git test
     }
 
     View.OnClickListener cl = new View.OnClickListener() {
