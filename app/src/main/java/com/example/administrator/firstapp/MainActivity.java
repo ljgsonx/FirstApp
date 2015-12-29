3a08a64a9f81543710a84a7303e3790870ec4c2c
liujingui <liujingui@baiwang.com>
2015-12-29 22:02:46 +0800

    git test

--- app/src/main/java/com/example/administrator/firstapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/firstapp/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("====");
         init();
         loginBtn.setOnClickListener(cl);
         loginCancel.setOnClickListener(cl);
